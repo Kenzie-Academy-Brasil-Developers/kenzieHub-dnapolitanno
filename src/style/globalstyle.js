d82104f0b312287d030f6fd1381a2f147e8c7884
dnapolitanno <dudadnapolitano@gmail.com>
2023-02-07 13:56:38 -0300

    style: created global style

---
+++ src/style/globalstyle.js
@@ -0,0 +1,35 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --color-primary: #FF577F;
+        --color-primary-focus: #FF427F;
+        --color-primary-negative: #59323F;
+        --color-grey-0: #F8F9FA;
+        --color-grey-1: #868E96;
+        --color-grey-2: #343B41;
+        --color-grey-3: #212529;
+        --color-grey-4: #121214;
+        --color-white: #ffffff;
+        
+        --color-back-modal: rgba(18, 18, 20, 0.5);
+        
+        --font-family: 'Inter', sans-serif;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        border: none;
+        outline: none;
+        box-sizing: border-box;
+        list-style: none;
+        text-decoration: none;
+    }
+
+    body {
+        font-family: var(--font-family);
+        background-color: var(--color-grey-4);
+        color: var(--color-grey-0);
+    }
+`;
